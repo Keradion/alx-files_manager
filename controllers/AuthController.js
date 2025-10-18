a3f80c44ca8306da81141bb9668709bab2349d8c
Daniel <danielshitaye10@gmail.com>
2025-10-18 03:54:55 -0700

    add user password comparision from the database hash and the given password in /connect endpoint

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,8 @@
 const dbClient = require('../utils/db.js');
-const token = require('../utils/token.js');
-const user = require('../utils//user.js');
-	
+const Token = require('../utils/token.js');
+const User = require('../utils/user.js');
+const Password = require('../utils/password.js');
+
 class AuthController{
 	
 	static async getConnect (request, response) {
@@ -10,7 +11,7 @@ class AuthController{
 		// and get the user email and password. ?
 
 		const authorizationToken = request.headers.authorization;
-		const email = await token.decodeAuthToken(authorizationToken);
+		const { email, password }  = await Token.decodeAuthToken(authorizationToken);
 		
 		// check if there is any user associated with the email provided
 		console.log(email.length, email);
@@ -24,10 +25,16 @@ class AuthController{
 			return;
 		}
 
+		// Password is invalid
+		if (! (await Password.validateHashPassword(password, user.password))) {
+			response.status(401).json({ 'error': 'Unauthorized' });
+                        return;
+		}
+
 		// Get an authorization token and set in redis
 		// so that we can keep track of the user ?
 
-		const authToken = await token.generateAuthToken(user._id);
+		const authToken = await Token.generateAuthToken(user._id);
 
 		// send back response
 
@@ -42,7 +49,7 @@ class AuthController{
 		// check if the user id and token has found 
 		// remove the token from redis and return true.
 
-		if (await user.disconnectUser(token)) {
+		if (await User.disconnectUser(token)) {
 			response.status(204).end();
 			return;
 		}
